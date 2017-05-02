dd43ff9a078ae67b6bd5cd377e2e3d906ebb826d
Guillaume Leclerc <guill.bout@gmail.com>
2017-05-02 22:57:29 +0800

    Refactor Harvest class with using guzzle

--- src/Harvest.class.php
+++ src/Harvest.class.php
@@ -1,4 +1,9 @@
 <?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
 /**
  *
  */
@@ -6,28 +11,23 @@ class Harvest
 {
     private $message;
     private $domain;
-    private $apiToken;
 
     public function __construct($domain = null, $apiToken = null)
     {
+        $this->client = new Client([
+            'base_uri' => 'https://' . $domain . '.harvestapp.com/daily/',
+            'headers' => [
+                'Content-type' => 'application/json',
+                'Accept' => 'application/json',
+                'Authorization' => 'Basic ' . $apiToken,
+            ],
+        ]);
         $this->message = '';
-        $this->domain = $domain;
-        $this->apiToken = $apiToken;
     }
 
     public function startTimer($description, $projectId, $taskId)
     {
         $harvestId = null;
-
-        $url = 'https://' . $this->domain . '.harvestapp.com/daily/add';
-
-        $base64Token = $this->apiToken;
-
-        $headers = [
-            "Content-type: application/json",
-            "Accept: application/json",
-            'Authorization: Basic ' . $base64Token,
-        ];
 
         $item = [
             'notes' => $description,
@@ -35,18 +35,14 @@ class Harvest
             'task_id' => $taskId,
         ];
 
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($item, true));
-        $response = curl_exec($ch);
-        $lastHttpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
+        $response = $this->client->post('add', [
+            'json' => $item,
+        ]);
 
-        if ($response === false || $lastHttpCode !== 201) {
+        if ($response->getStatusCode() !== 201) {
             $this->message = '- Cannot start Harvest timer!';
         } else {
-            $data = json_decode($response, true);
+            $data = json_decode($response->getBody(), true);
             $harvestId = $data['id'];
             $this->message = '- Harvest timer started';
         }
@@ -59,25 +55,9 @@ class Harvest
         $res = false;
 
         if ($this->isTimerRunning($timerId) === true) {
-            $url = 'https://' . $this->domain . '.harvestapp.com/daily/timer/' . $timerId;
+            $response = $this->client->get('timer/' . $timerId);
 
-            $base64Token = $this->apiToken;
-
-            $headers = [
-                "Content-type: application/json",
-                "Accept: application/json",
-                'Authorization: Basic ' . $base64Token,
-            ];
-
-            $ch = curl_init($url);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $response = curl_exec($ch);
-            $lastHttpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-            curl_close($ch);
-
-            if ($response === false || $lastHttpCode !== 200) {
+            if ($response->getStatusCode() !== 200) {
                 $this->message = '- Could not stop Harvest timer!';
             } else {
                 $this->message = '- Harvest timer stopped';
@@ -99,25 +79,9 @@ class Harvest
     {
         $res = false;
 
-        $url = 'https://' . $this->domain . '.harvestapp.com/daily/delete/' . $timerId;
+        $response = $this->client->delete('delete/' . $timerId);
 
-        $base64Token = $this->apiToken;
-
-        $headers = [
-            "Content-type: application/json",
-            "Accept: application/json",
-            'Authorization: Basic ' . $base64Token,
-        ];
-
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($ch);
-        $lastHttpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-
-        if ($response === false || $lastHttpCode !== 200) {
+        if ($response->getStatusCode() !== 200) {
             $this->message = '- Could not delete Harvest timer!';
         } else {
             $this->message = '- Harvest timer deleted';
@@ -131,26 +95,10 @@ class Harvest
     {
         $res = false;
 
-        $url = 'https://' . $this->domain . '.harvestapp.com/daily/show/' . $timerId;
+        $response = $this->client->get('show/' . $timerId);
 
-        $base64Token = $this->apiToken;
-
-        $headers = [
-            "Content-type: application/json",
-            "Accept: application/json",
-            'Authorization: Basic ' . $base64Token,
-        ];
-
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($ch);
-        $lastHttpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-
-        if ($response !== false && $lastHttpCode === 200) {
-            $data = json_decode($response, true);
+        if ($response->getStatusCode() === 200) {
+            $data = json_decode($response->getBody(), true);
             if (isset($data['timer_started_at']) === true) {
                 $res = true;
             }
