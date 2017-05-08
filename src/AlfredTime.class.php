315d54ca400f73b72102e824cf0094f4d03b998e
Guillaume Leclerc <guill.bout@gmail.com>
2017-05-09 02:53:33 +0800

    Ran AlfredTime through CodeFormatteer

--- src/AlfredTime.class.php
+++ src/AlfredTime.class.php
@@ -5,16 +5,35 @@ require 'Harvest.class.php';
 
 class AlfredTime
 {
+    /**
+     * @var mixed
+     */
     private $config;
-    private $message;
+
+    /**
+     * @var array
+     */
     private $currentImplementation = [
-        'start' => ['toggl'],
+        'start'         => ['toggl'],
         'start_default' => ['toggl', 'harvest'],
-        'stop' => ['toggl', 'harvest'],
-        'delete' => ['toggl'],
+        'stop'          => ['toggl', 'harvest'],
+        'delete'        => ['toggl'],
     ];
-    private $toggl;
+
+    /**
+     * @var mixed
+     */
     private $harvest;
+
+    /**
+     * @var mixed
+     */
+    private $message;
+
+    /**
+     * @var mixed
+     */
+    private $toggl;
 
     public function __construct()
     {
@@ -24,21 +43,206 @@ class AlfredTime
         $this->harvest = new Harvest($this->config['harvest']['domain'], $this->config['harvest']['api_token']);
     }
 
-    public function isConfigured()
+    /**
+     * @return mixed
+     */
+    public function activatedServices()
     {
-        return $this->config === null ? false : true;
+        $services = [];
+
+        if ($this->isTogglActive() === true) {
+            array_push($services, 'toggl');
+        }
+
+        if ($this->isHarvestActive() === true) {
+            array_push($services, 'harvest');
+        }
+
+        return $services;
     }
 
-    public function hasTimerRunning()
+    /**
+     * @param $timerId
+     * @return mixed
+     */
+    public function deleteTimer($timerId)
     {
-        return $this->config['workflow']['is_timer_running'] === false ? false : true;
+        $message = '';
+
+        $atLeastOneTimerDeleted = false;
+
+        foreach ($this->implementedServicesForFeature('delete') as $service) {
+            $functionName = 'delete' . ucfirst($service) . 'Timer';
+
+            if (call_user_func_array(['AlfredTime', $functionName], [$timerId]) === true) {
+                if ($timerId === $this->config['workflow']['timer_' . $service . '_id']) {
+                    $this->config['workflow']['timer_' . $service . '_id'] = null;
+                    $atLeastOneTimerDeleted = true;
+                }
+            }
+
+            $message .= $this->getLastMessage() . "\r\n";
+        }
+
+        if ($atLeastOneTimerDeleted === true) {
+            $this->config['workflow']['is_timer_running'] = false;
+            $this->saveConfiguration();
+        }
+
+        return $message;
     }
 
+    public function generateDefaultConfigurationFile()
+    {
+        $this->config = [
+            'workflow' => [
+                'is_timer_running'  => false,
+                'timer_toggl_id'    => null,
+                'timer_harvest_id'  => null,
+                'timer_description' => '',
+            ],
+            'toggl'    => [
+                'is_active'          => true,
+                'api_token'          => '',
+                'default_project_id' => '',
+                'default_tags'       => '',
+            ],
+            'harvest'  => [
+                'is_active'          => true,
+                'domain'             => '',
+                'api_token'          => '',
+                'default_project_id' => '',
+                'default_task_id'    => '',
+            ],
+        ];
+
+        $this->saveConfiguration();
+    }
+
+    /**
+     * @param $projectId
+     * @return mixed
+     */
+    public function getProjectName($projectId)
+    {
+        $projectName = '';
+
+        $projects = $this->getProjects();
+
+        foreach ($projects as $project) {
+            if ($project['id'] === $projectId) {
+                $projectName = $project['name'];
+                break;
+            }
+        }
+
+        return $projectName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProjects()
+    {
+        $projects = [];
+
+        if ($this->isTogglActive() === true) {
+            $projects = array_merge($projects, $this->getTogglProjects());
+        }
+
+        return $projects;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRecentTimers()
+    {
+        $timers = [];
+
+        if ($this->isTogglActive() === true) {
+            $timers = array_merge($timers, $this->getRecentTogglTimers());
+        }
+
+        return $timers;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTags()
+    {
+        $tags = [];
+
+        if ($this->isTogglActive() === true) {
+            $tags = array_merge($tags, $this->getTogglTags());
+        }
+
+        return $tags;
+    }
+
+    /**
+     * @return mixed
+     */
     public function getTimerDescription()
     {
         return $this->config['workflow']['timer_description'];
     }
 
+    /**
+     * @return mixed
+     */
+    public function hasTimerRunning()
+    {
+        return $this->config['workflow']['is_timer_running'] === false ? false : true;
+    }
+
+    /**
+     * @param $feature
+     * @return mixed
+     */
+    public function implementedServicesForFeature($feature = null)
+    {
+        $services = [];
+
+        if (isset($this->currentImplementation[$feature]) === true) {
+            $services = $this->currentImplementation[$feature];
+        }
+
+        return $services;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function isConfigured()
+    {
+        return $this->config === null ? false : true;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function servicesToUndo()
+    {
+        $services = [];
+
+        foreach ($this->activatedServices() as $service) {
+            if ($this->config['workflow']['timer_' . $service . '_id'] !== null) {
+                array_push($services, $service);
+            }
+        }
+
+        return $services;
+    }
+
+    /**
+     * @param $description
+     * @param $projectsDefault
+     * @param null $tagsDefault
+     * @param null $startDefault
+     * @return mixed
+     */
     public function startTimer($description = '', $projectsDefault = null, $tagsDefault = null, $startDefault = false)
     {
         $message = '';
@@ -46,7 +250,7 @@ class AlfredTime
         $atLeastOneServiceStarted = false;
         $implementedServices = $this->implementedServicesForFeature($startType);
 
-        /**
+        /*
          * When starting a new timer, all the services timer IDs have to be put to null
          * so that when the user uses the UNDO feature, it doesn't delete old previous
          * other services timers. The timer IDs are used for the UNDO feature and
@@ -67,6 +271,7 @@ class AlfredTime
             $functionName = 'start' . ucfirst($service) . 'Timer';
             $timerId = call_user_func_array(['AlfredTime', $functionName], [$description, $defaultProjectId, $defaultTags]);
             $this->config['workflow']['timer_' . $service . '_id'] = $timerId;
+
             if ($timerId !== null) {
                 $atLeastOneServiceStarted = true;
             }
@@ -83,21 +288,28 @@ class AlfredTime
         return $message;
     }
 
+    /**
+     * @param $description
+     * @return mixed
+     */
     public function startTimerWithDefaultOptions($description)
     {
         $projectsDefault = [
-            'toggl' => $this->config['toggl']['default_project_id'],
+            'toggl'   => $this->config['toggl']['default_project_id'],
             'harvest' => $this->config['harvest']['default_project_id'],
         ];
 
         $tagsDefault = [
-            'toggl' => $this->config['toggl']['default_tags'],
+            'toggl'   => $this->config['toggl']['default_tags'],
             'harvest' => $this->config['harvest']['default_task_id'],
         ];
 
         return $this->startTimer($description, $projectsDefault, $tagsDefault, true);
     }
 
+    /**
+     * @return mixed
+     */
     public function stopRunningTimer()
     {
         $message = '';
@@ -105,9 +317,11 @@ class AlfredTime
 
         foreach ($this->activatedServices() as $service) {
             $functionName = 'stop' . ucfirst($service) . 'Timer';
+
             if (call_user_func(['AlfredTime', $functionName]) === true) {
                 $atLeastOneServiceStopped = true;
             }
+
             $message .= $this->getLastMessage() . "\r\n";
         }
 
@@ -119,61 +333,9 @@ class AlfredTime
         return $message;
     }
 
-    public function generateDefaultConfigurationFile()
-    {
-        $this->config = [
-            'workflow' => [
-                'is_timer_running' => false,
-                'timer_toggl_id' => null,
-                'timer_harvest_id' => null,
-                'timer_description' => '',
-            ],
-            'toggl' => [
-                'is_active' => true,
-                'api_token' => '',
-                'default_project_id' => '',
-                'default_tags' => '',
-            ],
-            'harvest' => [
-                'is_active' => true,
-                'domain' => '',
-                'api_token' => '',
-                'default_project_id' => '',
-                'default_task_id' => '',
-            ],
-        ];
-
-        $this->saveConfiguration();
-    }
-
-    public function activatedServices()
-    {
-        $services = [];
-
-        if ($this->isTogglActive() === true) {
-            array_push($services, 'toggl');
-        }
-
-        if ($this->isHarvestActive() === true) {
-            array_push($services, 'harvest');
-        }
-
-        return $services;
-    }
-
-    public function servicesToUndo()
-    {
-        $services = [];
-
-        foreach ($this->activatedServices() as $service) {
-            if ($this->config['workflow']['timer_' . $service . '_id'] !== null) {
-                array_push($services, $service);
-            }
-        }
-
-        return $services;
-    }
-
+    /**
+     * @return mixed
+     */
     public function syncOnlineDataToLocalCache()
     {
         $message = '';
@@ -185,28 +347,9 @@ class AlfredTime
         return $message;
     }
 
-    public function getProjects()
-    {
-        $projects = [];
-
-        if ($this->isTogglActive() === true) {
-            $projects = array_merge($projects, $this->getTogglProjects());
-        }
-
-        return $projects;
-    }
-
-    public function getTags()
-    {
-        $tags = [];
-
-        if ($this->isTogglActive() === true) {
-            $tags = array_merge($tags, $this->getTogglTags());
-        }
-
-        return $tags;
-    }
-
+    /**
+     * @return mixed
+     */
     public function undoTimer()
     {
         $message = '';
@@ -216,8 +359,10 @@ class AlfredTime
         }
 
         $atLeastOneTimerDeleted = false;
+
         foreach ($this->servicesToUndo() as $service) {
             $functionName = 'delete' . ucfirst($service) . 'Timer';
+
             if (call_user_func_array(['AlfredTime', $functionName], [$this->config['workflow']['timer_' . $service . '_id']]) === true) {
                 $this->config['workflow']['timer_' . $service . '_id'] = null;
                 $atLeastOneTimerDeleted = true;
@@ -233,86 +378,10 @@ class AlfredTime
         return $message;
     }
 
-    public function getRecentTimers()
-    {
-        $timers = [];
-
-        if ($this->isTogglActive() === true) {
-            $timers = array_merge($timers, $this->getRecentTogglTimers());
-        }
-
-        return $timers;
-    }
-
-    public function deleteTimer($timerId)
-    {
-        $message = '';
-
-        $atLeastOneTimerDeleted = false;
-        foreach ($this->implementedServicesForFeature('delete') as $service) {
-            $functionName = 'delete' . ucfirst($service) . 'Timer';
-            if (call_user_func_array(['AlfredTime', $functionName], [$timerId]) === true) {
-                if ($timerId === $this->config['workflow']['timer_' . $service . '_id']) {
-                    $this->config['workflow']['timer_' . $service . '_id'] = null;
-                    $atLeastOneTimerDeleted = true;
-                }
-            }
-
-            $message .= $this->getLastMessage() . "\r\n";
-        }
-
-        if ($atLeastOneTimerDeleted === true) {
-            $this->config['workflow']['is_timer_running'] = false;
-            $this->saveConfiguration();
-        }
-
-        return $message;
-    }
-
-    public function implementedServicesForFeature($feature = null)
-    {
-        $services = [];
-
-        if (isset($this->currentImplementation[$feature]) === true) {
-            $services = $this->currentImplementation[$feature];
-        }
-
-        return $services;
-    }
-
-    public function getProjectName($projectId)
-    {
-        $projectName = '';
-
-        $projects = $this->getProjects();
-        foreach ($projects as $project) {
-            if ($project['id'] === $projectId) {
-                $projectName = $project['name'];
-                break;
-            }
-        }
-
-        return $projectName;
-    }
-
-    private function getRecentTogglTimers()
-    {
-        return $this->toggl->getRecentTimers();
-    }
-
-    private function getLastMessage()
-    {
-        return $this->message;
-    }
-
-    private function deleteTogglTimer($togglId)
-    {
-        $res = $this->toggl->deleteTimer($togglId);
-        $this->message = $this->toggl->getLastMessage();
-
-        return $res;
-    }
-
+    /**
+     * @param $harvestId
+     * @return mixed
+     */
     private function deleteHarvestTimer($harvestId)
     {
         $res = $this->harvest->deleteTimer($harvestId);
@@ -321,24 +390,93 @@ class AlfredTime
         return $res;
     }
 
-    private function syncTogglOnlineDataToLocalCache()
+    /**
+     * @param $togglId
+     * @return mixed
+     */
+    private function deleteTogglTimer($togglId)
     {
-        $data = $this->toggl->getOnlineData();
+        $res = $this->toggl->deleteTimer($togglId);
         $this->message = $this->toggl->getLastMessage();
 
-        if (empty($data) === false) {
-            $this->saveTogglDataCache($data);
-        }
+        return $res;
+    }
 
+    /**
+     * @return mixed
+     */
+    private function getLastMessage()
+    {
         return $this->message;
     }
 
-    private function saveTogglDataCache($data)
+    /**
+     * @return mixed
+     */
+    private function getRecentTogglTimers()
     {
-        $cacheFile = getenv('alfred_workflow_data') . '/toggl_cache.json';
-        file_put_contents($cacheFile, json_encode($data));
+        return $this->toggl->getRecentTimers();
     }
 
+    /**
+     * @return mixed
+     */
+    private function getTogglProjects()
+    {
+        $cacheData = [];
+        $cacheFile = getenv('alfred_workflow_data') . '/toggl_cache.json';
+
+        if (file_exists($cacheFile)) {
+            $cacheData = json_decode(file_get_contents($cacheFile), true);
+        }
+
+        /*
+         * To only show projects that are currently active
+         * The Toggl API is slightly weird on that
+         */
+        foreach ($cacheData['data']['projects'] as $key => $project) {
+            if (isset($project['server_deleted_at']) === true) {
+                unset($cacheData['data']['projects'][$key]);
+            }
+        }
+
+        return $cacheData['data']['projects'];
+    }
+
+    /**
+     * @return mixed
+     */
+    private function getTogglTags()
+    {
+        $cacheFile = getenv('alfred_workflow_data') . '/toggl_cache.json';
+        $cacheData = [];
+
+        if (file_exists($cacheFile)) {
+            $cacheData = json_decode(file_get_contents($cacheFile), true);
+        }
+
+        return $cacheData['data']['tags'];
+    }
+
+    /**
+     * @return mixed
+     */
+    private function isHarvestActive()
+    {
+        return $this->config['harvest']['is_active'];
+    }
+
+    /**
+     * @return mixed
+     */
+    private function isTogglActive()
+    {
+        return $this->config['toggl']['is_active'];
+    }
+
+    /**
+     * @return mixed
+     */
     private function loadConfiguration()
     {
         $config = null;
@@ -363,24 +501,21 @@ class AlfredTime
         file_put_contents($configFile, json_encode($this->config, JSON_PRETTY_PRINT));
     }
 
-    private function startTogglTimer($description, $projectId, $tagNames)
+    /**
+     * @param $data
+     */
+    private function saveTogglDataCache($data)
     {
-        $togglId = $this->toggl->startTimer($description, $projectId, $tagNames);
-        $this->message = $this->toggl->getLastMessage();
-
-        return $togglId;
+        $cacheFile = getenv('alfred_workflow_data') . '/toggl_cache.json';
+        file_put_contents($cacheFile, json_encode($data));
     }
 
-    private function stopTogglTimer()
-    {
-        $togglId = $this->config['workflow']['timer_toggl_id'];
-
-        $res = $this->toggl->stopTimer($togglId);
-        $this->message = $this->toggl->getLastMessage();
-
-        return $res;
-    }
-
+    /**
+     * @param $description
+     * @param $projectId
+     * @param $taskId
+     * @return mixed
+     */
     private function startHarvestTimer($description, $projectId, $taskId)
     {
         $harvestId = $this->harvest->startTimer($description, $projectId, $taskId);
@@ -389,6 +524,23 @@ class AlfredTime
         return $harvestId;
     }
 
+    /**
+     * @param $description
+     * @param $projectId
+     * @param $tagNames
+     * @return mixed
+     */
+    private function startTogglTimer($description, $projectId, $tagNames)
+    {
+        $togglId = $this->toggl->startTimer($description, $projectId, $tagNames);
+        $this->message = $this->toggl->getLastMessage();
+
+        return $togglId;
+    }
+
+    /**
+     * @return mixed
+     */
     private function stopHarvestTimer()
     {
         $harvestId = $this->config['workflow']['timer_harvest_id'];
@@ -399,47 +551,31 @@ class AlfredTime
         return $res;
     }
 
-    private function getTogglProjects()
+    /**
+     * @return mixed
+     */
+    private function stopTogglTimer()
     {
-        $cacheData = [];
-        $cacheFile = getenv('alfred_workflow_data') . '/toggl_cache.json';
+        $togglId = $this->config['workflow']['timer_toggl_id'];
 
-        if (file_exists($cacheFile)) {
-            $cacheData = json_decode(file_get_contents($cacheFile), true);
-        }
+        $res = $this->toggl->stopTimer($togglId);
+        $this->message = $this->toggl->getLastMessage();
 
-        /**
-         * To only show projects that are currently active
-         * The Toggl API is slightly weird on that
-         */
-        foreach ($cacheData['data']['projects'] as $key => $project) {
-            if (isset($project['server_deleted_at']) === true) {
-                unset($cacheData['data']['projects'][$key]);
-            }
-        }
-
-        return $cacheData['data']['projects'];
+        return $res;
     }
 
-    private function getTogglTags()
+    /**
+     * @return mixed
+     */
+    private function syncTogglOnlineDataToLocalCache()
     {
-        $cacheFile = getenv('alfred_workflow_data') . '/toggl_cache.json';
-        $cacheData = [];
+        $data = $this->toggl->getOnlineData();
+        $this->message = $this->toggl->getLastMessage();
 
-        if (file_exists($cacheFile)) {
-            $cacheData = json_decode(file_get_contents($cacheFile), true);
+        if (empty($data) === false) {
+            $this->saveTogglDataCache($data);
         }
 
-        return $cacheData['data']['tags'];
-    }
-
-    private function isTogglActive()
-    {
-        return $this->config['toggl']['is_active'];
-    }
-
-    private function isHarvestActive()
-    {
-        return $this->config['harvest']['is_active'];
+        return $this->message;
     }
 }
