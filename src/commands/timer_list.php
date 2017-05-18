31bd994936737e55d18b7441bbbfba85e2e44396
Guill Lo <guill.bout@gmail.com>
2017-05-18 05:41:54 +0000

    Apply fixes from StyleCI

--- src/commands/timer_list.php
+++ src/commands/timer_list.php
@@ -7,7 +7,6 @@ use AlfredTime\Config;
 use Alfred\Workflows\Workflow;
 use AlfredTime\WorkflowHandler;
 
-
 $workflow = new Workflow();
 $config = new Config(getenv('alfred_workflow_data') . '/config.json');
 $workflowHandler = new WorkflowHandler($config);
