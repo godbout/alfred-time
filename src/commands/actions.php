4f1c30290e19e3cd417b24232e2af9ff2c5bc208
Guillaume Leclerc <guill.bout@gmail.com>
2017-06-01 16:58:23 +0800

    Remove useless code in actions.php

--- src/commands/actions.php
+++ src/commands/actions.php
@@ -3,15 +3,13 @@
 switch ($data['action']) {
     case 'config':
         $config->generateDefaultConfigurationFile();
-        // no break
+    // no break
     case 'edit':
         exec('open "' . getenv('alfred_workflow_data') . '/config.json"');
         break;
 
     case 'sync':
         $message = $workflowHandler->syncOnlineDataToLocalCache();
-        echo $message;
-        exit();
         break;
 
     case 'undo':
@@ -19,14 +17,10 @@ switch ($data['action']) {
             $timer->undo(),
             'undo'
         );
-        echo $message;
-        exit();
         break;
 
     case 'stop':
         $message = $workflowHandler->getNotification($timer->stop(), 'stop');
-        echo $message;
-        exit();
         break;
 
     case 'final':
@@ -46,15 +40,17 @@ switch ($data['action']) {
                 'delete'
             );
         } elseif ($data['original_action'] === 'continue') {
+            $timerInfo = $data['timer_info'];
             $message = $workflowHandler->getNotification(
                 $timer->start(
-                    $data['timer_info']['description'],
-                    [$data['timer_info']['service'] => $data['timer_info']['project_id']],
-                    [$data['timer_info']['service'] => $data['timer_info']['tags']],
-                    $data['timer_info']['service']),
+                    $timerInfo['description'],
+                    [$timerInfo['service'] => $timerInfo['project_id']],
+                    [$timerInfo['service'] => $timerInfo['tags']],
+                    $timerInfo['service']),
                 'start'
             );
         }
+
         break;
 }
 
